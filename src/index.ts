0f04a34c399326034ba7005f7d9c127ae820ce66
Adam - The Developer <adam@sala.co>
2024-11-25 11:24:00 +0700

    updated error handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 export function convertToKhmerNumbers(input: number | string): string {
-    // Define a mapping of Arabic numerals to Khmer numerals
-    const khmerDigits = ['០', '១', '២', '៣', '៤', '៥', '៦', '៧', '៨', '៩'];
+  if (!input) {
+    throw new TypeError("No input received!")
+  }
+  // Define a mapping of Arabic numerals to Khmer numerals
+  const khmerDigits = ["០", "១", "២", "៣", "៤", "៥", "៦", "៧", "៨", "៩"];
 
-    // Convert the input to a string and replace each digit
-    return input.toString().split('').map(digit => {
-        const num = parseInt(digit, 10);
-        return isNaN(num) ? digit : khmerDigits[num];
-    }).join('');
+  // Convert the input to a string and replace each digit
+  return input
+    .toString()
+    .split("")
+    .map((digit) => {
+      const num = parseInt(digit, 10);
+      return isNaN(num) ? digit : khmerDigits[num];
+    })
+    .join("");
 }
